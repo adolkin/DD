b586fa0d0a6265b5e3198a5040d8834db3f249bf
adolkin <tuanbk189@gmail.com>
2018-01-08 16:01:46 +1100

    Add Bootstrap

--- src/app/drag-drop/drag-drop.module.ts
+++ src/app/drag-drop/drag-drop.module.ts
@@ -4,12 +4,13 @@ import { DragDropRoutingModule } from './drag-drop-routing.module';
 import { SharedModule } from './../shared/shared.module';
 
 import { DragDropComponent } from './drag-drop.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports: [
     DragDropRoutingModule,
     SharedModule
   ],
-  declarations: [DragDropComponent]
+  declarations: [DragDropComponent, ModalComponent]
 })
 export class DragDropModule { }
